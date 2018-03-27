3346c742494f74a0422423ff785e005f8d89110b
張君豪 <f74066365@mail.ncku.edu.tw>
2018-03-27 23:56:18 +0800

    change 1

--- play.cpp
+++ play.cpp
@@ -3,21 +3,29 @@
 using namespace std;
 int main() {
     cout<<"1 3 C 4 6 7 8 5"<<endl;
+	cout<<"1 1 6 25"<<endl;
+	cout<<"1 3 6 25"<<endl;
+	cout<<"1 4 6 25"<<endl;
+	cout<<"1 C 6 25"<<endl;
+	cout<<"1 5 6 25"<<endl;
+	cout<<"1 7 6 25"<<endl;
+	cout<<"1 8 6 25"<<endl;
+    cout<<"1 6 6 25"<<endl;
+    cout<<"0"<<endl;
     string line;
     while(getline(cin,line)) {
         if(line=="END") {
-            cout<<"1 1 5 22"<<endl;
-            cout<<"1 3 16 22"<<endl;
-            cout<<"1 C 5 22"<<endl;
-            cout<<"1 4 16 22"<<endl;
-            cout<<"1 6 5 22"<<endl;
-            cout<<"1 7 16 22"<<endl;
-            cout<<"1 8 5 22"<<endl;
-            cout<<"1 5 16 22"<<endl;
+            cout<<"1 1 6 25"<<endl;
+            cout<<"1 3 6 25"<<endl;
+            cout<<"1 4 6 25"<<endl;
+            cout<<"1 C 6 25"<<endl;
+            cout<<"1 5 6 25"<<endl;
+            cout<<"1 7 6 25"<<endl;
+            cout<<"1 8 6 25"<<endl;
+            cout<<"1 6 6 25"<<endl;
             cout<<"0"<<endl;
         }
 
     }
-
     return 0;
 }
