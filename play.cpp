c6565d0b81c1b710b725de72f51b5a1a3241b13f
張君豪 <f74066365@mail.ncku.edu.tw>
2018-03-28 00:13:53 +0800

    play3

--- play.cpp
+++ play.cpp
@@ -3,15 +3,6 @@
 using namespace std;
 int main() {
     cout<<"1 3 C 4 6 7 8 5"<<endl;
-	cout<<"1 3 6 25"<<endl;
-	cout<<"1 1 6 25"<<endl;
-	cout<<"1 4 6 25"<<endl;
-	cout<<"1 C 6 25"<<endl;
-	cout<<"1 5 6 25"<<endl;
-	cout<<"1 7 6 25"<<endl;
-	cout<<"1 8 6 25"<<endl;
-    cout<<"1 6 6 25"<<endl;
-    cout<<"0"<<endl;
     string line;
     while(getline(cin,line)) {
         if(line=="END") {
@@ -25,7 +16,6 @@ int main() {
             cout<<"1 6 6 25"<<endl;
             cout<<"0"<<endl;
         }
-
     }
     return 0;
 }
