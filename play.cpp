b91ae659b2db6f94e4a13d4c178b261e9cbd366d
張君豪 <f74066365@mail.ncku.edu.tw>
2018-03-28 00:35:00 +0800

    play6

--- play.cpp
+++ play.cpp
@@ -5,15 +5,15 @@ int main() {
     cout<<"1 3 C 4 6 7 8 5"<<endl;
     string line;
     while(getline(cin,line)) {
-        if(line=="END") {
-            cout<<"1 1 6 24"<<endl;
-            cout<<"1 3 6 24"<<endl;
-            cout<<"1 4 6 24"<<endl;
-            cout<<"1 C 6 24"<<endl;
-            cout<<"1 5 6 24"<<endl;
-            cout<<"1 7 6 24"<<endl;
-            cout<<"1 8 6 24"<<endl;
-            cout<<"1 6 6 24"<<endl;
+        if(line=="BEGIN") {
+            cout<<"1 1 14 24"<<endl;
+            cout<<"1 3 14 24"<<endl;
+            cout<<"1 4 14 24"<<endl;
+            cout<<"1 C 14 24"<<endl;
+            cout<<"1 5 14 24"<<endl;
+            cout<<"1 7 14 24"<<endl;
+            cout<<"1 8 14 24"<<endl;
+            cout<<"1 6 14 24"<<endl;
             cout<<"0"<<endl;
         }
     }
