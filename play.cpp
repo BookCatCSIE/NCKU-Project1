77f8c9ea6519511af9d813adcd76bcf1577acca5
張君豪 <f74066365@mail.ncku.edu.tw>
2018-03-29 00:30:47 +0800

    add play22

--- play.cpp
+++ play.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main() {
     cout<<"1 3 C 4 6 7 2 9"<<endl;
     string line;
-    int i=1;
+    int i=0;
     while(getline(cin,line)) {
         if(line=="END") {
-            if(i%4==2||i%4==0) {
+            i=i+1;
+            if(i%3==2) {
                 cout<<"1 1 10 9"<<endl;
                 cout<<"1 3 10 9"<<endl;
                 cout<<"1 4 10 9"<<endl;
@@ -17,7 +18,7 @@ int main() {
                 cout<<"1 9 10 9"<<endl;
                 cout<<"1 6 10 9"<<endl;
                 cout<<"0"<<endl;
-            } else if(i%4==1) {
+            } else if(i%3==1) {
                 cout<<"1 1 3 23"<<endl;
                 cout<<"1 4 3 23"<<endl;
                 cout<<"1 C 3 23"<<endl;
@@ -27,18 +28,18 @@ int main() {
                 cout<<"1 9 3 23"<<endl;
                 cout<<"1 6 3 23"<<endl;
                 cout<<"0"<<endl;
-            } else {
-                cout<<"1 1 8 23"<<endl;
-                cout<<"1 4 8 23"<<endl;
-                cout<<"1 C 8 23"<<endl;
-                cout<<"1 7 8 23"<<endl;
-                cout<<"1 2 8 23"<<endl;
-                cout<<"1 3 8 23"<<endl;
-                cout<<"1 9 8 23"<<endl;
-                cout<<"1 6 8 23"<<endl;
+            } else if(i%3==0) {
+                cout<<"1 1 8 22"<<endl;
+                cout<<"1 4 8 22"<<endl;
+                cout<<"1 C 8 22"<<endl;
+                cout<<"1 7 8 22"<<endl;
+                cout<<"1 2 8 22"<<endl;
+                cout<<"1 3 8 22"<<endl;
+                cout<<"1 9 8 22"<<endl;
+                cout<<"1 6 8 22"<<endl;
                 cout<<"0"<<endl;
             }
-            ++i;
+
         }
     }
     return 0;
