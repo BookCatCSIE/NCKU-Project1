4d4902e37b47a814afda148725759f848a6301d8
張君豪 <f74066365@mail.ncku.edu.tw>
2018-03-28 22:28:58 +0800

    add play15

--- play.cpp
+++ play.cpp
@@ -28,14 +28,14 @@ int main() {
                 cout<<"1 6 3 23"<<endl;
                 cout<<"0"<<endl;
             } else {
-                cout<<"1 1 18 23"<<endl;
-                cout<<"1 4 18 23"<<endl;
-                cout<<"1 C 18 23"<<endl;
-                cout<<"1 7 18 23"<<endl;
-                cout<<"1 8 18 23"<<endl;
-                cout<<"1 3 18 23"<<endl;
-                cout<<"1 5 18 23"<<endl;
-                cout<<"1 6 18 23"<<endl;
+                cout<<"1 1 8 23"<<endl;
+                cout<<"1 4 8 23"<<endl;
+                cout<<"1 C 8 23"<<endl;
+                cout<<"1 7 8 23"<<endl;
+                cout<<"1 8 8 23"<<endl;
+                cout<<"1 3 8 23"<<endl;
+                cout<<"1 5 8 23"<<endl;
+                cout<<"1 6 8 23"<<endl;
                 cout<<"0"<<endl;
             }
             ++i;
