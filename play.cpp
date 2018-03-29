806768b0c7f3f6807d7a33847ab12b58af6386e9
張君豪 <f74066365@mail.ncku.edu.tw>
2018-03-29 23:39:44 +0800

    play25

--- play.cpp
+++ play.cpp
@@ -2,44 +2,54 @@
 #include <string>
 using namespace std;
 int main() {
-    cout<<"1 3 C 4 6 7 2 9"<<endl;
+    cout<<"1 3 C 6 4 8 2 9"<<endl;
     string line;
     int i=0;
     while(getline(cin,line)) {
         if(line=="END") {
             i=i+1;
-            if(i%3==2) {
+            if(i%6==5||i%6==0) {
+                cout<<"1 4 10 9"<<endl;
                 cout<<"1 1 10 9"<<endl;
                 cout<<"1 3 10 9"<<endl;
-                cout<<"1 4 10 9"<<endl;
+
+
                 cout<<"1 C 10 9"<<endl;
-                cout<<"1 2 10 9"<<endl;
-                cout<<"1 7 10 9"<<endl;
-                cout<<"1 9 10 9"<<endl;
                 cout<<"1 6 10 9"<<endl;
+
+
                 cout<<"0"<<endl;
-            } else if(i%3==1) {
+            } else if(i%6==2) {
                 cout<<"1 1 3 23"<<endl;
-                cout<<"1 4 3 23"<<endl;
+                cout<<"1 8 3 23"<<endl;
                 cout<<"1 C 3 23"<<endl;
-                cout<<"1 7 3 23"<<endl;
-                cout<<"1 2 3 23"<<endl;
                 cout<<"1 3 3 23"<<endl;
-                cout<<"1 9 3 23"<<endl;
+
+                cout<<"1 4 3 23"<<endl;
+                cout<<"1 9 10 1"<<endl;
                 cout<<"1 6 3 23"<<endl;
                 cout<<"0"<<endl;
-            } else if(i%3==0) {
-                cout<<"1 1 8 22"<<endl;
-                cout<<"1 4 8 22"<<endl;
-                cout<<"1 C 8 22"<<endl;
-                cout<<"1 7 8 22"<<endl;
-                cout<<"1 2 8 22"<<endl;
-                cout<<"1 3 8 22"<<endl;
-                cout<<"1 9 8 22"<<endl;
-                cout<<"1 6 8 22"<<endl;
+            } else if(i%6==1) {
+                cout<<"1 8 8 21"<<endl;
+                cout<<"1 C 8 21"<<endl;
+                cout<<"1 1 8 21"<<endl;
+
+                cout<<"1 3 8 21"<<endl;
+                cout<<"1 6 8 21"<<endl;
+
+                cout<<"1 4 8 21"<<endl;
+                cout<<"0"<<endl;
+            } else if(i%6==3||i%6==4) {
+
+                cout<<"1 4 7 10"<<endl;
+                cout<<"1 1 7 10"<<endl;
+
+                cout<<"1 C 7 10"<<endl;
+                cout<<"1 3 7 10"<<endl;
+                cout<<"1 6 7 10"<<endl;
+                cout<<"1 2 7 10"<<endl;
                 cout<<"0"<<endl;
             }
-
         }
     }
     return 0;
